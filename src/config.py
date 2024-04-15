5673e4dfc01db78ad4dbd02fd05afb1c4ff71c6e
reas <reas.qf0@gmail.com>
2024-04-15 14:20:30 +0300

    fix config.json parameters not working

--- src/config.py
+++ src/config.py
@@ -1,7 +1,13 @@
 import os
 import json
 
-path = lambda x: os.path.abspath(x)
+types = {
+    "config_file": os.path.abspath,
+    "ffmpeg_path": str,
+    "fps": float,
+    "thrn": int,
+    "font": os.path.abspath
+}
 
 default_values = {
     "config_file": "config.json",
@@ -10,40 +16,31 @@ default_values = {
     "thrn": 4
 }
 
-types = {
-    "config_file": path,
-    "ffmpeg_path": str,
-    "fps": float,
-    "thrn": int,
-    "font": path
-}
-
 
 class Config:
     values = {}
 
     @staticmethod
     def set(key, value):
-        Config.values[key] = types[key](value)
+        if key not in Config.values:
+            Config.values[key] = types[key](value)
 
     @staticmethod
     def init(argv):
-        # 1. default values
-        for key in default_values:
-            Config.set(key, default_values[key])
-
-        # 2. values from argv
+        # 1. values from argv
         for i in range(1, len(argv), 2):
             if not argv[i].startswith('--'):
                 break
             Config.set(argv[i][2:], argv[i + 1])
 
-        # 3. values from config_file
-        if 'config_file' in Config.values:
-            with open(Config.values['config_file']) as file:
-                json_values = json.load(file)
-            for key in json_values:
-                if key not in Config.values:
-                    Config.set(key, json_values[key])
+        # 2. values from config_file
+        with open(Config.values['config_file'] if 'config_file' in Config.values else default_values['config_file']) as file:
+            json_values = json.load(file)
+        for key in json_values:
+            Config.set(key, json_values[key])
+
+        # 3. default values
+        for key in default_values:
+            Config.set(key, default_values[key])
 
         return i
